7337de89f2794f8e447e2499419675f5dd2f6d5d
Jordan Robinson <hello@jordanrobinson.org>
2025-11-28 09:16:48 +0000

    update email host to be fetched from .env

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -183,7 +183,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 if LOCALDEV:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.fastmail.com"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_BROADCASTS = ""
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
